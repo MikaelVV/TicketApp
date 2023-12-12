51090703ea8d7ed8797101bd3620769f4735bed0
kfdjhenoi2 <125042496+kfdjhenoi2@users.noreply.github.com>
2023-12-12 15:20:11 +0200

    Add files via upload

--- HuvipuistoKayttoliittyma.java
+++ HuvipuistoKayttoliittyma.java
@@ -1,27 +1,26 @@
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.Integer;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.text.SimpleDateFormat;
-import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.Scanner;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+// Lisää Kokonaismyynnit-luokka
+class Kokonaismyynnit {
+    public static int laskuri = 0;
+}
 
 public class HuvipuistoKayttoliittyma extends JFrame {
     private static JTextField lapsenNimi;
     private static JTextField huoltajanNumero;
     private static JComboBox<String> alennusRyhmaComboBox;
-
+    private static JButton normaaliButton;
+    private static JButton lastenButton;
 
     public HuvipuistoKayttoliittyma() {
         setTitle("Huvipuiston Rannekesovellus");
@@ -29,29 +28,29 @@ public class HuvipuistoKayttoliittyma extends JFrame {
         setSize(800, 550);
         setLayout(new GridLayout(3, 2));
 
-
-        JButton normaaliButton = new JButton("<html>Normaali<br>Hinta: 22€</html>");
+        normaaliButton = new JButton("<html>Normaali<br>Hinta: 22€</html>");
         normaaliButton.setPreferredSize(new Dimension(120, 60));
         normaaliButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
-
-                if(e.getSource()==normaaliButton){
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == normaaliButton) {
+                    int lippujenMaara = kysyLippujenMaara();
                     normaaliButton.setEnabled(false);
-                    int normaaliHinta = 22;
+                    int normaaliHinta = 22 * lippujenMaara;
                     Kokonaismyynnit.laskuri += normaaliHinta;
                 }
             }
         });
 
-        JButton lastenButton = new JButton("<html>Lasten<br>Hinta: 15€</html>");
+        lastenButton = new JButton("<html>Lasten<br>Hinta: 15€</html>");
         lastenButton.setPreferredSize(new Dimension(120, 60));
         lastenButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
-                if(e.getSource()==lastenButton){
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == lastenButton) {
+                    int lippujenMaara = kysyLippujenMaara();
                     lastenButton.setEnabled(false);
-                    int lastenHinta = 15;
+                    int lastenHinta = 15 * lippujenMaara;
                     Kokonaismyynnit.laskuri += lastenHinta;
                 }
             }
@@ -85,19 +84,25 @@ public class HuvipuistoKayttoliittyma extends JFrame {
         add(tulostaButton);
         tulostaButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 Kuitti();
                 Kokonaismyynnit();
+
+                // Nollaa napit ja tekstikentät
+                normaaliButton.setEnabled(true);
+                lastenButton.setEnabled(true);
+                lapsenNimi.setText("");
+                huoltajanNumero.setText("");
+                alennusRyhmaComboBox.setSelectedIndex(0);
             }
         });
-
 
         JButton ostaButton = new JButton("Osta");
         ostaButton.setPreferredSize(new Dimension(80, 30));
         add(ostaButton);
         ostaButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 Kokonaismyynnit();
                 Kuitti();
             }
@@ -106,77 +111,82 @@ public class HuvipuistoKayttoliittyma extends JFrame {
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new HuvipuistoKayttoliittyma());
-
+    private int kysyLippujenMaara() {
+        try {
+            String lippujenMaaraStr = JOptionPane.showInputDialog("Syötä lippujen määrä:");
+            if (lippujenMaaraStr != null) {
+                return Integer.parseInt(lippujenMaaraStr);
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Virheellinen syöte. Anna lippujen määrä numeroina.");
+        }
+        return 0;
     }
 
-    private static void Kuitti(){
-        try{
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new HuvipuistoKayttoliittyma());
+    }
+
+    private static void Kuitti() {
+        try {
             // Luodaan lipukkeiden tekstitiedosto.
             File kuitti = new File("Huvipuistolipukkeet.txt");
-            kuitti.createNewFile();
 
-            //Luodaan kirjoittaja, jolla päästään kirjoittamaan tekstitiedostoon tekstiä.
-            FileWriter kirjoittaja = new FileWriter("Huvipuistolipukkeet.txt");
-
-            // Määritellään tämänhetkinen päivämäärä ja kellon aika.
-            LocalDate paivaMaara = LocalDate.now();
-            LocalTime aika = LocalTime.now();
-
-            String lapsenNimiteksti = lapsenNimi.getText();
-            String huoltajanNumeroteksti = huoltajanNumero.getText();
-
-            String tulostus = "Kuitti\n" + paivaMaara + " "+ aika + "\nLapsen nimi: " + lapsenNimiteksti + "\nHuoltajan puhelin numero: "+ huoltajanNumeroteksti;
-
-            kirjoittaja.write(tulostus);
-            kirjoittaja.close();
-
-            if(kuitti.createNewFile()){
+            if (kuitti.createNewFile()) {
                 System.out.println("Lippukuitti on luotu: " + kuitti.getName());
                 System.out.println("Tiedosto polku: " + kuitti.getAbsolutePath());
-            }else{
+            } else {
                 System.out.println("Lippu on luotu jo.");
                 System.out.println("Tiedosto polku: " + kuitti.getAbsolutePath());
             }
-        } catch (IOException e){
+
+            // Luodaan kirjoittaja, jolla päästään kirjoittamaan tekstitiedostoon tekstiä.
+            try (FileWriter kirjoittaja = new FileWriter(kuitti)) {
+                // Määritellään tämänhetkinen päivämäärä ja kellon aika.
+                LocalDate paivaMaara = LocalDate.now();
+                LocalTime aika = LocalTime.now();
+
+                String lapsenNimiteksti = lapsenNimi.getText();
+                String huoltajanNumeroteksti = huoltajanNumero.getText();
+
+                String tulostus = "Kuitti\n" + paivaMaara + " " + aika + "\nLapsen nimi: " + lapsenNimiteksti
+                        + "\nHuoltajan puhelin numero: " + huoltajanNumeroteksti;
+
+                kirjoittaja.write(tulostus);
+            }
+        } catch (IOException e) {
             System.out.println("Lippukuittia luodessa tapahtui virhe.");
             e.printStackTrace();
         }
     }
 
-    private static void Kokonaismyynnit(){
-        try{
+    private static void Kokonaismyynnit() {
+        try {
             File kokonaismyynnit = new File("Kokonaismyynnit.txt");
-            kokonaismyynnit.createNewFile();
 
-            FileWriter kirjoittaja = new FileWriter("Kokonaismyynnit.txt");
-            BufferedWriter lisaaja = new BufferedWriter(kirjoittaja);
-
-
-
-            LocalDate paivaMaara = LocalDate.now();
-            LocalTime aika = LocalTime.now();
-            int laskuri = 0;
-            String laskuriTeksti = Integer.toString(laskuri);
-            String kokonaisTulostus = "Kokonaismyyntitilanne \n" + paivaMaara + " " + aika + "\n" + laskuriTeksti;
-
-            kirjoittaja.write(kokonaisTulostus);
-            lisaaja.write("Lisatty");
-            kirjoittaja.close();
-            lisaaja.close();
-
-            if(kokonaismyynnit.createNewFile()){
+            if (kokonaismyynnit.createNewFile()) {
                 System.out.println("Kokonaismyyntituloste on luotu: " + kokonaismyynnit.getName());
                 System.out.println("Tiedosto polku: " + kokonaismyynnit.getAbsolutePath());
-            }else{
+            } else {
                 System.out.println("Kokonaismyyntituloste on luotu jo.");
                 System.out.println("Tiedosto polku: " + kokonaismyynnit.getAbsolutePath());
             }
-        }catch(IOException e){
+
+            // Luodaan kirjoittaja, jolla päästään kirjoittamaan tekstitiedostoon tekstiä.
+            try (FileWriter kirjoittaja = new FileWriter(kokonaismyynnit);
+                    BufferedWriter lisaaja = new BufferedWriter(kirjoittaja)) {
+
+                LocalDate paivaMaara = LocalDate.now();
+                LocalTime aika = LocalTime.now();
+                String laskuriTeksti = Integer.toString(Kokonaismyynnit.laskuri);
+                String kokonaisTulostus = "Kokonaismyyntitilanne \n" + paivaMaara + " " + aika + "\n" + laskuriTeksti;
+
+                kirjoittaja.write(kokonaisTulostus);
+                lisaaja.write("Lisatty");
+            }
+        } catch (IOException e) {
             System.out.println("Kokonaismyyntitulostetta luodessa tapahtui virhe.");
             e.printStackTrace();
         }
     }
-
 }
